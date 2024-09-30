785c950b4c187a9eb2999bcaa55aa2436b8456ea
Christopher Contreras <112120990+upnextcontreras@users.noreply.github.com>
2024-09-29 18:36:03 -0700

    Create testcase.cpp

---
+++ testcase.cpp
@@ -0,0 +1,18 @@
+// Test Case 1: Simple Variable Declaration
+int a = 5;
+float b = 10.0;
+
+// Test Case 2: Control Structures
+if (x > 5) {
+    output x;
+}
+else {
+    input y;
+}
+
+// Test Case 3: Loops and Functions
+while (i < 10) {
+    do {
+        function();
+    } whileend;
+}
